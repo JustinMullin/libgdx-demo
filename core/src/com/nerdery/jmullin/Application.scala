d8f3b6c330f8fb8d2dc51007ffb8cc8cd02ed826
Justin Mullin <justinamullin@gmail.com>
2015-09-24 13:23:56 -0500

    Reworked Duke tracking in Application to allow multiple Dukes at once.  Set up a timer to periodically create a new Duke at the top of the screen.  Cleaned up Duke's shape so it somewhat accurately traces his outline.

--- core/src/com/nerdery/jmullin/Application.scala
+++ core/src/com/nerdery/jmullin/Application.scala
@@ -4,20 +4,34 @@ import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.World
+import com.badlogic.gdx.utils.Timer
 import com.badlogic.gdx.{ApplicationAdapter, Gdx}
 
+import scala.util.Random
+
 class Application extends ApplicationAdapter {
+  val random = new Random
+
   lazy val batch = new SpriteBatch()
 
-  val world = new World(new Vector2(0, -6), true)
-  val duke = new Duke(world)
+  val world = new World(new Vector2(0, -10), true)
+
+  val dukes = collection.mutable.Buffer[Duke]()
 
   override def create(): Unit = {
-    duke.create()
+    Timer.schedule(new Timer.Task {
+      override def run(): Unit = createDuke()
+    }, 0.5f, 0.5f)
 
     new Barrier(world,
       new Vector2(0, 0).mul(World.screenToWorld),
       new Vector2(Gdx.graphics.getWidth, 0).mul(World.screenToWorld))
+  }
+
+  def createDuke(): Unit = {
+    val duke = new Duke(world, new Vector2(random.nextFloat()*12f-6f, 14))
+    duke.create()
+    dukes.append(duke)
   }
 
   override def render(): Unit = {
@@ -28,7 +42,7 @@ class Application extends ApplicationAdapter {
 
     batch.begin()
 
-    duke.draw(batch)
+    dukes.foreach(_.draw(batch))
 
     batch.end()
   }
--- core/src/com/nerdery/jmullin/Duke.scala
+++ core/src/com/nerdery/jmullin/Duke.scala
@@ -5,18 +5,25 @@ import com.badlogic.gdx.graphics.g2d.{Sprite, SpriteBatch}
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d._
 
-class Duke(world: World) {
+class Duke(world: World, startPosition: Vector2) {
   lazy val sprite = new Sprite(new Texture("duke.png"))
 
   val screenPosition = new Vector2()
 
   val bodyDef = new BodyDef
   bodyDef.`type` = BodyDef.BodyType.DynamicBody
+  bodyDef.position.set(startPosition)
 
   val body = world.createBody(bodyDef)
 
-  val shape = new CircleShape()
-  shape.setRadius(2)
+  val shape = new PolygonShape()
+  shape.set(Array(
+    new Vector2(-1.2f, -1.5f),
+    new Vector2(1.2f, -1.5f),
+    new Vector2(1.2f, 0),
+    new Vector2(0, 1.5f),
+    new Vector2(-1.2f, 0)
+  ))
 
   val fixtureDef = new FixtureDef
   fixtureDef.shape = shape
@@ -33,7 +40,7 @@ class Duke(world: World) {
   def draw(batch: SpriteBatch): Unit = {
     screenPosition.set(body.getPosition.mul(World.worldToScreen))
     sprite.setPosition(screenPosition.x-sprite.getWidth/2f, screenPosition.y-sprite.getHeight/2f)
-    sprite.setRotation(body.getTransform.getRotation)
+    sprite.setRotation(body.getAngle*180f/Math.PI.toFloat)
     sprite.draw(batch)
   }
 }
