214e1613647577af2926b275a76a04df8209a4a3
Justin Mullin <justinamullin@gmail.com>
2015-09-24 12:01:28 -0500

    Add Duke image, create a Sprite and Texture from the asset, then draw it!

--- core/src/com/nerdery/jmullin/Application.scala
+++ core/src/com/nerdery/jmullin/Application.scala
@@ -1,15 +1,25 @@
 package com.nerdery.jmullin
 
-import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.g2d.{Sprite, SpriteBatch}
+import com.badlogic.gdx.graphics.{GL20, Texture}
 import com.badlogic.gdx.{ApplicationAdapter, Gdx}
 
 class Application extends ApplicationAdapter {
+  lazy val batch = new SpriteBatch()
+  lazy val duke = new Sprite(new Texture("duke.png"))
+
   override def create(): Unit = {
 
   }
 
   override def render(): Unit = {
-    Gdx.gl.glClearColor(0, 0, 0, 1)
+    Gdx.gl.glClearColor(1f, 1f, 1f, 1)
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+    batch.begin()
+
+    duke.draw(batch)
+
+    batch.end()
   }
 }
